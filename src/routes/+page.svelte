3a4369ce04d1f6a5c9903f67282972f56d7b02ca
Marcello Annicchiarico <marcello.annicchiarico@eva.global>
2024-09-25 14:10:34 +0200

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,43 @@
 <script lang="ts">
-  import { fly, slide } from "svelte/transition";
+  import { fly } from "svelte/transition";
   import { enhance } from "$app/forms";
   import Cart from "$lib/components/Cart.svelte";
-  import Scrolview from "$lib/components/Scrolview.svelte";
 
   export let form;
 
   let creating = false;
+
+  import { onMount, onDestroy } from "svelte";
+  let listElement: any;
+
+  let items: any[] = [];
+
+  for (let index = 0; index < 100; index++) {
+    items = [...items, `item ${index}`];
+  }
+
+  function loadMore() {
+    for (var index = 0; index < 100; index++) {
+      items = [...items, `item ${items.length}`];
+    }
+  }
+
+  onMount(() => {
+    if (listElement) {
+      listElement.addEventListener("scroll", function () {
+        if (
+          listElement.scrollTop + listElement.clientHeight >=
+          listElement.scrollHeight - 100
+        ) {
+          loadMore();
+        }
+      });
+    }
+  });
+
+  onDestroy(() => {
+    if (listElement) listElement.removeEventListener("scroll");
+  });
 </script>
 
 <section>
@@ -31,7 +62,7 @@
             value={form?.description ?? ""}
             autocomplete="off"
             required
-            placeholder="Min 350"
+            placeholder=""
             type="number"
           />
         </label>
@@ -39,7 +70,31 @@
       </form>
     </svelte:fragment>
     <svelte:fragment slot="body">
-      <Scrolview></Scrolview>
+      <div class="table">
+        <table bind:this={listElement}>
+          <thead>
+            <tr>
+              <td>Personalnummer</td>
+              <td>Vorname</td>
+              <td>Nachname</td>
+              <td>Aktiv</td>
+              <td>Erstellt</td>
+            </tr>
+          </thead>
+          <tbody>
+            {#each items as item, index}
+              <tr class:isodd={index % 2}>
+                <td>14924</td>
+                <td>Abigayle</td>
+                <td>Mills</td>
+                <td>Aktiv</td>
+                <td>August 7, 2017</td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+        <div class="footer">50 geladen von 350 Ergebnissen</div>
+      </div>
     </svelte:fragment>
   </Cart>
 </section>
@@ -67,6 +122,54 @@
           color: gray;
         }
       }
+    }
+    .table {
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+      inset: 0;
+    }
+    table {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      border-collapse: collapse;
+      overflow: auto;
+      thead {
+        position: sticky;
+        top: 0;
+        background-color: var(--bg-secondary);
+      }
+      tbody {
+        flex: 1;
+        color: gray;
+        tr {
+          &:hover {
+            td {
+              background-color: var(--color-hover);
+            }
+          }
+        }
+        .isodd td {
+          background-color: var(--bg-contrast-low);
+        }
+      }
+      thead,
+      tbody {
+        tr {
+          display: table;
+          width: 100%;
+          td {
+            padding: 1em 1em;
+            width: 20%;
+          }
+        }
+      }
+    }
+    .footer {
+      color: gray;
+      font-size: 0.8em;
+      padding: 1em;
     }
   }
 </style>
